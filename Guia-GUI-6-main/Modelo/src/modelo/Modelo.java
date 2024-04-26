bd8759685bbfbaaa80ca375526e531fda6e57251
MiguelDev <migopa0222@gmail.com>
2024-04-25 22:24:49 -0500

    GUIA 6 Terminada

---
+++ Guia-GUI-6-main/Modelo/src/modelo/Modelo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author migop
+ */
+public class Modelo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal5 mirar = new Principal5();
+        
+        mirar.setLocationRelativeTo(null);
+        mirar.setVisible(true);
+    }
+    
+}
